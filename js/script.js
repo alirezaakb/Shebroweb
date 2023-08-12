7dcf51cdbc32cd1b75db268b3ac3055111dd64d4
Alireza Akbari <alireza10akbari@gmail.com>
2023-08-12 14:20:18 +0330

    solve contact form & responsive problems

--- js/script.js
+++ js/script.js
@@ -6,16 +6,3 @@ const btnCloseContact = document.querySelector(".close-contact-btn");
 btnNavEl.addEventListener("click", function () {
     headerEl.classList.toggle('nav-open');
 });
-
-const btnContactEl= document.querySelector(".contact-btn");
-
-const contactEl = document.querySelector(".contact");
-
-btnContactEl.addEventListener("click", function () {
-    contactEl.classList.toggle("contact-open");
-});
-
-btnCloseContact.addEventListener("click" , function (){
-    contactEl.classList.remove("contact-open");
-})
-
