6da0f2ad58891297472625143dc4bdcd7da426ff
Alireza Akbari <alireza10akbari@gmail.com>
2023-08-12 11:09:24 +0330

    add contact section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 const btnNavEl = document.querySelector(".btn-nav-mobile");
 console.log(btnNavEl)
 const headerEl = document.querySelector(".header");
+const btnCloseContact = document.querySelector(".close-contact-btn");
 
 btnNavEl.addEventListener("click", function () {
     headerEl.classList.toggle('nav-open');
 });
+
+const btnContactEl= document.querySelector(".contact-btn");
+
+const contactEl = document.querySelector(".contact");
+
+btnContactEl.addEventListener("click", function () {
+    contactEl.classList.toggle("contact-open");
+});
+
+btnCloseContact.addEventListener("click" , function (){
+    contactEl.classList.remove("contact-open");
+})
+
